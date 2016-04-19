5192065d3450fb38017104961b6e5ce1ec7cefce
Lance Fallon <lfallon@Lances-Mac-mini.local>
2016-04-19 12:28:44 -0400

    including multiple same elements, handling dup id's, using the monitor

--- app/src/main/java/com/training/lfallon/viewincludesdemo/MainActivity.java
+++ app/src/main/java/com/training/lfallon/viewincludesdemo/MainActivity.java
@@ -8,24 +8,31 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private View button;
+    private View topButton;
+    private View bottomButton;
     private TextView textView;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.include_button_panel_button);
         textView = (TextView)findViewById(R.id.include_text_panel_textView);
 
-        button.setOnClickListener(this);
+        View topButtonPanel = findViewById(R.id.activity_main_buttonPanelTop);
+        View bottomButtonPanel = findViewById(R.id.activity_main_buttonPanelBottom);
+
+        topButton = topButtonPanel.findViewById(R.id.include_button_panel_button);
+        bottomButton = bottomButtonPanel.findViewById(R.id.include_button_panel_button);
+        topButton.setOnClickListener(this);
+        bottomButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.include_button_panel_button){
-            textView.setText("Clicked it!");
+            textView.setText("Clicked it!" + (counter++));
         }
     }
 }
