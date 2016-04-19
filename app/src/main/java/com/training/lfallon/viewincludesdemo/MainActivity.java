62ab4a2d49ccb4fec52d08e54c001049ec1af38e
Lance Fallon <lfallon@Lances-Mac-mini.local>
2016-04-19 11:50:05 -0400

    includes and stuff

--- app/src/main/java/com/training/lfallon/viewincludesdemo/MainActivity.java
+++ app/src/main/java/com/training/lfallon/viewincludesdemo/MainActivity.java
@@ -2,12 +2,30 @@ package com.training.lfallon.viewincludesdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private View button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.include_button_panel_button);
+        textView = (TextView)findViewById(R.id.include_text_panel_textView);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.include_button_panel_button){
+            textView.setText("Clicked it!");
+        }
     }
 }
